11007ca931e163dde81a70c207e7d206fc562449
Wey <tasuke567@gmail.com>
2025-02-04 16:14:01 +0700

    delete file ode.js/express

--- src/types.ts
+++
@@ -1,9 +0,0 @@
-// src/types.ts
-export interface Post {
-    id: string;
-    userId: string;
-    content: string;
-    createdAt: string;
-    updatedAt: string;
-  }
-  
--- src/types/User.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    id: string;
-    name: string;
-    // Add other fields as needed
-    email?: string;
-    username?: string;
-    // Any other user-related properties
-  }
